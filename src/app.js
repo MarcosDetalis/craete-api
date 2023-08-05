a926456481a9bdce9f7b9c20c85936ecb8b4bb3e
gustavoriv <103583993+gustavoriv@users.noreply.github.com>
2023-08-05 15:24:07 -0400

    cambios para usuarios

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import bibliotecaPoint from "./routes/reservas.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 import solicitudPoint from "./routes/solicitudes.routes.js"
+import usuariosPoint from "./routes/usuarios.routes.js"
 const app = express();
 // Middlewares
 app.use(morgan("dev"));
