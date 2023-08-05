6f0d9a705dc9b6df1fb93a002ed26ba24f75ab20
gustavoriv <103583993+gustavoriv@users.noreply.github.com>
2023-08-05 15:21:18 -0400

    se anadio usuarios

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/", indexRoutes);
 app.use("/api", bibliotecaPoint);
 app.use("/soli", solicitudPoint);
-
+app.use("/usu",usuariosPoint);
 app.use((req, res, next) => {
   res.status(404).json({ message: "No enontrado" });
 });
