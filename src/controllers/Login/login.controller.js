62cc1a24d6edc95e835ac8311681601182e5fdf3
MarcosDetalis <detalismarcos@gmail.com>
2023-10-01 21:51:49 -0300

    jwt

--- src/controllers/Login/login.controller.js
+++ src/controllers/Login/login.controller.js
@@ -1,17 +1,45 @@
 import { pool } from "../../db.js";
+import  jwt from "jsonwebtoken";
 
 export const getLogin = async (req, res) => {
-    
-    const { correo, pass } = req.body;
-    const [result] = await pool.query(
-      `SELECT * from usuarios where  usu_correo= "${correo}" and contrasena ="${pass}"`
-    );
-        
-    if (result.length > 0) {
-        console.log(result);
-         res.status(201).json({ message: "Acceso Correcto" }); 
-    } else {
-       res.status(500).json({ message: "Error al autenticarse" });   
-    }
+  const { correo, pass } = req.body;
+  const [result] = await pool.query(
+    `SELECT * from usuarios where  usu_correo= "${correo}" and contrasena ="${pass}"`
+  );
+  if (result.length > 0) {
+    console.log(result);
+      const token = jwt.sign(
+        { id: correo },'opop',
+        {
+          expiresIn: 86, // 24 hours
+        }  
+      );
+   res.header("auth-token", token).json({
+     error: null,
+     data: { token },
+   });
+      await pool.query(
+        `UPDATE usuarios SET token="${token}" WHERE usu_correo= "${correo}";`
+      );
+
+      console.log(token);
      
+  } else {
+    res.status(500).json({ message: "Error al autenticarse" });
+  }
 };
+export const gettoket = async (req, res) => {
+ const { token } = req.body;
+ const [result] = await pool.query(
+   `SELECT token from usuarios where  token= "${token}" `
+ );
+  if (result.length > 0) {
+    res.status(200).json({ message: result });
+    console.log("login",result)
+  } else {
+    res.status(500).json({ message: "Error al autenticarse" });
+  }
+
+};
+
+
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getLogin } from "../controllers/Login/login.controller.js";
+import { getLogin, gettoket } from "../controllers/Login/login.controller.js";
 
 const router = Router();
 
  
 
 router.post("/auten", getLogin);
-
+ 
+router.post("/toket", gettoket);
 
 export default router;
